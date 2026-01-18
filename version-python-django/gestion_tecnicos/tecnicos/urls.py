c4e5465ec6361f56f67aa93077af4cd8f185129e
Daniel F. Rincón <danyforce11@hotmail.com>
2026-01-18 18:56:13 -0500

    Implementación de uuarios y permisos correspondientes

--- version-python-django/gestion_tecnicos/tecnicos/urls.py
+++ version-python-django/gestion_tecnicos/tecnicos/urls.py
@@ -3,7 +3,8 @@ from .views import index
 from . import views
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', views.listado, name='listado'),
+    path('panel/', views.index, name='index'),
     path('editar/<int:id>/', views.editar, name='editar'),
     path('eliminar/<int:id>/', views.eliminar, name='eliminar'),
 ]
--- version-python-django/gestion_tecnicos/tecnicos/views.py
+++ version-python-django/gestion_tecnicos/tecnicos/views.py
@@ -4,50 +4,60 @@ from .forms import TecnicoForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
+from django.contrib.auth.decorators import login_required, user_passes_test
+
+
+def es_admin(user):
+    return user.is_superuser
 
 @never_cache
 @login_required
+@user_passes_test(es_admin)
 def index(request):
-    tecnicos = Tecnico.objects.all().order_by('-fecha_registro')
-
     if request.method == 'POST':
         form = TecnicoForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Técnico agregado exitosamente.')
+            messages.success(request, 'Técnico registrado correctamente.')
             return redirect('index')
     else:
         form = TecnicoForm()
 
-    context = {
+    tecnicos = Tecnico.objects.all()
+    return render(request, 'tecnicos/index.html', {
         'form': form,
         'tecnicos': tecnicos
-    }
-    return render(request, 'tecnicos/index.html', context)
-
-@never_cache
-@login_required
-def editar(request, id):
-    tecnico = get_object_or_404(Tecnico, id=id)
-
-    if request.method == 'POST':
-        form = TecnicoForm(request.POST, instance=tecnico)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Técnico actualizado exitosamente.')
-            return redirect('index')
-    else:
-        form = TecnicoForm(instance=tecnico)
-
-    return render(request, 'tecnicos/editar.html', {
-        'form': form,
-        'tecnico': tecnico
     })
 
+
 @never_cache
 @login_required
+@user_passes_test(es_admin)
+def editar(request, id):
+    tecnico = get_object_or_404(Tecnico, id=id)
+    form = TecnicoForm(request.POST or None, instance=tecnico)
+
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Datos actualizados.')
+        return redirect('index')
+
+    return render(request, 'tecnicos/editar.html', {'form': form})
+
+
+@never_cache
+@login_required
+@user_passes_test(es_admin)
 def eliminar(request, id):
     tecnico = get_object_or_404(Tecnico, id=id)
     tecnico.delete()
     messages.success(request, 'Técnico eliminado exitosamente.')
     return redirect('index')
+
+
+@login_required
+def listado(request):
+    tecnicos = Tecnico.objects.all()
+    return render(request, 'tecnicos/listado.html', {
+        'tecnicos': tecnicos
+    })
